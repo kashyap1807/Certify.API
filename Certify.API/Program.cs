37cd0917a7e3ff9746736d325449b79e2e6629d4
kashyap1807 <kashyapchudasama0@gmail.com>
2025-09-14 23:23:18 +0530

    Development & Program.js

--- Certify.API/Program.cs
+++ Certify.API/Program.cs
@@ -1,24 +1,47 @@
 
+using Certify.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 namespace Certify.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            #region Configuration Services - start
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<CertifyContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DbContext"),
+                    providerOptions => providerOptions.EnableRetryOnFailure());
+                
+            });
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            #endregion Configuration Services - end
 
+            #region Configuration Middleware - start
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            //if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference(options => {
+                    options.WithTitle("My API");
+                    options.WithTheme(ScalarTheme.Default);
+                    options.WithSidebar(true);
+                });
+
+                app.UseSwaggerUi(options =>
+                {
+                    options.DocumentPath = "openapi/v1.json"; 
+                });
             }
 
             app.UseHttpsRedirection();
@@ -29,6 +52,7 @@ namespace Certify.API
             app.MapControllers();
 
             app.Run();
+            #endregion Configuration Middleware - end
         }
     }
 }
